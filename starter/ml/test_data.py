fdcd1411eb56c0dfe71c0a6e89def0ad6538e9b6
Tessa van der Heiden <tessavdheiden@gmail.com>
2022-12-08 16:21:37 +0100

    training file and test

--- starter/ml/test_data.py
+++ starter/ml/test_data.py
@@ -1,34 +1,54 @@
 import pandas as pd
 import pytest
+import numpy as np
 import pickle
+
+import sklearn.preprocessing
+
 from starter.ml.model import train_model, compute_model_metrics, inference
+from starter.ml.data import process_data
+from sklearn.model_selection import train_test_split
 
 
 @pytest.fixture
 def data():
-    """ Read csv file """
-    df = pd.read_csv("./data/clean_census.csv")
+    # """ Read csv file """
+    # df = pd.read_csv("./data/clean_census.csv")
+    # return df
+    """ Simple function to generate some fake Pandas data."""
+    df = pd.DataFrame(
+        {
+            "id": [1, 2, 3],
+            "numeric_feat": [3.14, 2.72, 1.62],
+            "categorical_feat": ["dog", "dog", "cat"],
+        }
+    )
     return df
 
 
-# @pytest.fixture
-# def model():
-#     """ Load model """
-#     filename = './model/model_params.pkl'
-#     loaded_model = pickle.load(open(filename, "rb"))
-#     return loaded_model
+def test_process_data_return_data_type(data):
+    train, test = train_test_split(data, test_size=0.20)
+    X_test, y_test, _, _ = process_data(
+        test, categorical_features=["categorical_feat"], label="id", training=True
+    )
+    assert type(X_test) == np.ndarray
+    assert type(y_test) == np.ndarray
+    assert np.all(X_test[:, 0] > 0)
+
+
+def test_process_data_return_model_type(data):
+    train, test = train_test_split(data, test_size=0.20)
+    _, _, encoder, lb = process_data(
+        test, categorical_features=["categorical_feat"], label="id", training=True
+    )
+    assert type(encoder) == sklearn.preprocessing.OneHotEncoder
+    assert type(lb) == sklearn.preprocessing.LabelBinarizer
 
 
 def test_train_model(data):
-    """ If your data is assumed to have no null values then this is a valid test. """
-    model = train_model(X_train=X_train, y_train=y_train)
-
-
-
-# def test_slice_averages(data):
-#     """ Test to see if our mean per categorical slice is in the range 1.5 to 2.5."""
-#     for cat_feat in data["categorical_feat"].unique():
-#         avg_value = data[data["categorical_feat"] == cat_feat]["numeric_feat"].mean()
-#         assert (
-#             2.5 > avg_value > 1.5
-#         ), f"For {cat_feat}, average of {avg_value} not between 2.5 and 3.5."
+    train, test = train_test_split(data, test_size=0.20)
+    X_train, y_train, encoder, lb = process_data(
+        train, ["categorical_feat"], label="id", training=True
+    )
+    model = train_model(X_train, y_train)
+    assert type(model) == sklearn.linear_model.LogisticRegression
--- starter/train_model.py
+++ starter/train_model.py
@@ -3,10 +3,19 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+import pickle
+from ml.data import process_data
+from ml.model import train_model
 
 # Add code to load in the data.
+import os
+print(os.getcwd())
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+data = pd.read_csv("./data/clean_census.csv")
+
+# Optional enhancement, use K-fold cross validation instead of a
+# train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -24,5 +33,12 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train=X_train, y_train=y_train)
+filename = './model/model_params.pkl'
+pickle.dump(model, open(filename, "wb"))
